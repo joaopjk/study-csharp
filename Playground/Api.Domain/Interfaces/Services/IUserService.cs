98211356d1fdc485d3e83e2bd78a3697f05e4c20
joaopjk <joaopjk48@gmail.com>
2023-11-26 14:36:46 -0300

    IUserService

---
+++ Playground/Api.Domain/Interfaces/Services/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using Api.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Api.Domain.Interfaces.Services
+{
+    public interface IUserService
+    {
+        Task<UserEntity> Get(Guid id);
+        Task<IEnumerable<UserEntity>> GetAll();
+        Task<UserEntity> Post (UserEntity entity);
+        Task<UserEntity> Put (UserEntity entity);
+        Task<bool> Delete (Guid id);
+    }
+}
