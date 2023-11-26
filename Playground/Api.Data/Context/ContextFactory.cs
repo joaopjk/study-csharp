179d080460829dd0689f40ef03642368462c35a6
joaopjk <joaopjk48@gmail.com>
2023-11-26 12:05:32 -0300

    Add initial migration

--- Playground/Api.Data/Context/ContextFactory.cs
+++ Playground/Api.Data/Context/ContextFactory.cs
@@ -7,10 +7,10 @@ namespace Api.Data.Context
     {
         public MyContext CreateDbContext(string[] args)
         {
-            var connectionString = "Server=localhost;Port=3306;Database=mysqldb;Uid=sa;Pwd=sa@1234";
+            var connectionString = @"Server=localhost,3306;Database=mysqldb;Uid=root;Pwd=root@1234";
 
             var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
-            optionsBuilder.UseSqlServer(connectionString);
+            optionsBuilder.UseMySql(connectionString);
 
             return new MyContext(optionsBuilder.Options);
         }
