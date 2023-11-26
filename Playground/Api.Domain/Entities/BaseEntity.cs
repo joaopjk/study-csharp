c1fd4999a78c7899da5843f84b6fd51803140963
joaopjk <joaopjk48@gmail.com>
2023-11-26 11:10:16 -0300

    Add EntityFramework components
    Instalação dos componentes do EF necessário para o projeto

--- Playground/Api.Domain/Entities/BaseEntity.cs
+++ Playground/Api.Domain/Entities/BaseEntity.cs
@@ -1,12 +1,13 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Api.Domain.Entities
 {
-    public class BaseEntity
+    public abstract class BaseEntity
     {
+        [Key]
         public Guid Id { get; set; }
         private DateTime? _createAt;
-
         public DateTime? CreateAt
         {
             get { return _createAt; }
