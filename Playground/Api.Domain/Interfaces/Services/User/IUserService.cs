412de8d2bbd63d5de9cfd436f74e871f54eeeb96
joaopjk <joaopjk48@gmail.com>
2023-11-26 14:48:00 -0300

    IUserService

--- Playground/Api.Domain/Interfaces/Services/User/IUserService.cs
+++ Playground/Api.Domain/Interfaces/Services/User/IUserService.cs
@@ -3,14 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace Api.Domain.Interfaces.Services
+namespace Api.Domain.Interfaces.Services.User
 {
     public interface IUserService
     {
         Task<UserEntity> Get(Guid id);
         Task<IEnumerable<UserEntity>> GetAll();
-        Task<UserEntity> Post (UserEntity entity);
-        Task<UserEntity> Put (UserEntity entity);
-        Task<bool> Delete (Guid id);
+        Task<UserEntity> Post(UserEntity entity);
+        Task<UserEntity> Put(UserEntity entity);
+        Task<bool> Delete(Guid id);
     }
 }
---
+++ Playground/Api.Service/Services/UserService.cs
@@ -0,0 +1,44 @@
+﻿using Api.Domain.Entities;
+using Api.Domain.Interfaces;
+using Api.Domain.Interfaces.Services.User;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Api.Service.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IRepository<UserEntity> _userRepository;
+
+        public UserService(IRepository<UserEntity> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            return await _userRepository.DeleteAsync(id);
+        }
+
+        public async Task<UserEntity> Get(Guid id)
+        {
+            return await _userRepository.SelectAsync(id);
+        }
+
+        public async Task<IEnumerable<UserEntity>> GetAll()
+        {
+            return await _userRepository.SelectAsync();
+        }
+
+        public async Task<UserEntity> Post(UserEntity entity)
+        {
+            return await _userRepository.InsertAsync(entity);
+        }
+
+        public async Task<UserEntity> Put(UserEntity entity)
+        {
+            return await _userRepository.UpdateAsync(entity);
+        }
+    }
+}
