470e66942f46ea8cb821a87bca1e9fbc0e6ebcaa
joaopjk <joaopjk48@gmail.com>
2023-11-26 19:42:27 -0300

    Configure UsersControllers

---
+++ Playground/Api.Application/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using Api.Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Api.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> GetAll([FromServices] IUserService service)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                return Ok(await service.GetAll());
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+    }
+}
