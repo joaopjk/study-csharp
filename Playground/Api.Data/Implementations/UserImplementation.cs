c0d72ca5d6bdd369d9572165b98f56a2b810f683
joaopjk <joaopjk48@gmail.com>
2023-12-02 12:40:38 -0300

    Implement find by email

---
+++ Playground/Api.Data/Implementations/UserImplementation.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Api.Data.Context;
+using Api.Data.Repositories;
+using Api.Domain.Entities;
+using Api.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data.Implementations
+{
+    public class UserImplementation: BaseRepository<UserEntity>, IUserRepository
+    {
+        private readonly DbSet<UserEntity> _dbSet;
+
+        public UserImplementation(MyContext context) : base(context)
+        {
+            _dbSet = context.Set<UserEntity>();
+        }
+
+        public async Task<UserEntity> FindByLogin(string email)
+        {
+            return await _dbSet.FirstOrDefaultAsync(u => u.Email.Equals(email));
+        }
+    }
+}
