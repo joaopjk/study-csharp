0b779d45d37af96531164694cb06274e349c892b
joaopjk <joaopjk48@gmail.com>
2023-12-02 12:05:43 -0300

    Add swagger

--- Playground/Api.Application/Startup.cs
+++ Playground/Api.Application/Startup.cs
@@ -1,9 +1,11 @@
+using System;
 using Api.CrossCutting.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace Api.Application
 {
@@ -22,6 +24,21 @@ namespace Api.Application
             ConfigureRepository.ConfigureDependenciesRepository(services);
 
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Version = "v1",
+                    Title = "Api DDD",
+                    Description = "DDD",
+                    TermsOfService = new Uri("https://www.google.com"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "JOAO CICERO",
+                        Email = "joao@teste.com"
+                    }
+                });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -29,14 +46,17 @@ namespace Api.Application
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API DDD");
+                    c.RoutePrefix = string.Empty;
+                });
             }
-
+            
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
