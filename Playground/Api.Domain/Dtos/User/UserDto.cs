0574358bf44448675200818712a3bd569cb18834
joaopjk <joaopjk48@gmail.com>
2023-12-17 09:50:07 -0300

    Users dtos

---
+++ Playground/Api.Domain/Dtos/User/UserDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Domain.Dtos.User
+{
+    public class UserDto
+    {
+        [Required(ErrorMessage ="O campo nome é obrigatório!")]
+        [StringLength(60, ErrorMessage = "Nome deve ter no máximo {1} caracteres.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "O campo email é obrigátorio!")]
+        [EmailAddress(ErrorMessage = "Email em formato inválido!")]
+        public string Email { get; set; }
+    }
+}
