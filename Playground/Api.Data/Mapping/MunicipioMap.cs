b9b81edeff7542ca0492c8da0e9b8bd963243594
joaopjk <joaopjk48@gmail.com>
2023-12-17 22:24:32 -0300

    Implementação dos novos Map para EF

---
+++ Playground/Api.Data/Mapping/MunicipioMap.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Api.Data.Mapping
+{
+    public class MunicipioMap : IEntityTypeConfiguration<MunicipioEntity>
+    {
+        public void Configure(EntityTypeBuilder<MunicipioEntity> builder)
+        {
+            builder.ToTable("Municipio");
+            builder.HasKey(m => m.Id);
+            builder.HasIndex(m => m.CodIbge);
+            builder.HasOne(u => u.Uf)
+                   .WithMany(m => m.Municipios);
+        }
+    }
+}
