e683ad35e7985b7831071e6c696ac584d10e40be
joaopjk <joaopjk48@gmail.com>
2023-12-19 06:56:57 -0300

    UserService QuandoForExecutadoDelete Test

---
+++ Playground/Api.Service.Tests/User/QuandoForExecutadoDelete.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Api.Domain.Interfaces.Services.User;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Service.Tests.User
+{
+    public class QuandoForExecutadoDelete : UserTests
+    {
+        private IUserService _service;
+        private Mock<IUserService> _serviceMock;
+
+        [Fact]
+        public async Task E_possivel_executar_delete()
+        {
+            _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup(m => m.Delete(IdUsuario)).ReturnsAsync(true);
+            _service = _serviceMock.Object;
+
+            var result = await _service.Delete(IdUsuario);
+            Assert.True(result);
+
+            _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup(m => m.Delete(It.IsAny<Guid>())).ReturnsAsync(false);
+            _service = _serviceMock.Object;
+
+            result = await _service.Delete(It.IsAny<Guid>());
+            Assert.False(result);
+        }
+    }
+}
---
+++ Playground/Api.Service.Tests/User/QuandoForExecutadoUpdate.cs
@@ -0,0 +1,26 @@
+﻿using Api.Domain.Interfaces.Services.User;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Service.Tests.User
+{
+    public class QuandoForExecutadoUpdate : UserTests
+    {
+        private IUserService _service;
+        private Mock<IUserService> _serviceMock;
+
+        [Fact]
+        public async Task E_possivel_executar_put()
+        {
+            _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup(m => m.Put(UserUpdate)).ReturnsAsync(UserUpdateResult);
+            _service = _serviceMock.Object;
+
+            var result = await _service.Put(UserUpdate);
+            Assert.NotNull(result);
+            Assert.Equal(UserUpdateResult.Name, result.Name);
+            Assert.Equal(UserUpdateResult.Email, result.Email);
+        }
+    }
+}
